8209f5e9ee2b46dd4f354769131f0887e3b47916
Manju Rajashekhar <manj@twitter.com>
2012-10-12 13:46:56 -0700

    updated code to myapp

---
+++ java/myapp/src/main/java/com/mycompany/app/AppHelper.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: manj
+ * Date: 9/13/12
+ * Time: 2:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AppHelper {
+}
---
+++ java/myapp/src/main/java/com/mycompany/app/AppThread.java
@@ -0,0 +1,11 @@
+package com.mycompany.app;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: manj
+ * Date: 9/26/12
+ * Time: 2:54 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AppThread {
+}
